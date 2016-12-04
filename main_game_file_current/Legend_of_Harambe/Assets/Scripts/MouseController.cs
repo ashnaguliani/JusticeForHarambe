6f65c8e64b2f4f92ab387d214c2dde325a4aed88
cocr1742 <cory.cranford@colorado.edu>
2016-12-04 12:26:42 -0700

    Adding updated character control script
    Adding death/collecting script

--- main_game_file_current/Legend_of_Harambe/Assets/Scripts/MouseController.cs
+++ main_game_file_current/Legend_of_Harambe/Assets/Scripts/MouseController.cs
@@ -4,49 +4,51 @@ using System.Collections;
 public class MouseController : MonoBehaviour {
     public float jetpackForce = 75.0f;
     public float forwardMovementSpeed = 3.0f;
-	float timeNow = 0;
-	int distanceNow = 0;
+    private bool dead = false;
+    private uint bananas = 0;
 
+    void CollectBanana(Collider2D bananaCollider)
+    {
+        bananas++;
+        Destroy(bananaCollider.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("bananas"))
+            CollectBanana(collider);
+        else
+            HitByLaser(collider);
+    }
+    void HitByLaser(Collider2D laserCollider)
+    {
+        dead = true;
+    }
 	// Use this for initialization
 	void Start () {
-		AudioListener.volume = 1.0f;
+	
 	}
-
 	void FixedUpdate()
     {
         bool jetpackActive = Input.GetButton("Fire1");
+
+        jetpackActive = jetpackActive && !dead;
+
         if (jetpackActive)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jetpackForce));
         }
-        Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
-        newVelocity.x = forwardMovementSpeed;
-        GetComponent<Rigidbody2D>().velocity = newVelocity;
-    }
+        if (!dead)
+        {
+            Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
+            newVelocity.x = forwardMovementSpeed;
+            GetComponent<Rigidbody2D>().velocity = newVelocity;
+        }
+        //UpdateGroundedStatus();
+        //AdjustJetpack(jetpackActive);*/
+     }
 	// Update is called once per frame
 	void Update () {
-		//if (dead == false) {
-			timeNow = Time.timeSinceLevelLoad;
-			timeNow = (timeNow * 2);
-			distanceNow = (int)timeNow;
-		//}
-	}
-	void DisplayDistance()
-	{
-		Rect distanceIconRect = new Rect(0, 10, 32, 32);
-		//GUI.DrawTexture(coinIconRect, coinIconTexture);                         
-
-		GUIStyle style = new GUIStyle();
-		style.fontSize = 30;
-		style.fontStyle = FontStyle.Bold;
-		style.normal.textColor = Color.white;
-
-		Rect labelRect = new Rect(distanceIconRect.xMax, distanceIconRect.y, 60, 32);
-		GUI.Label(labelRect, distanceNow.ToString() + " meters", style);
-	}
-	void OnGUI()
-	{
-		DisplayDistance ();
-
+	
 	}
 }
