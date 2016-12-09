1f9d1e192bc19b2f55451ab780cf48d49da621e8
ksotebeer <ksotebeer95@gmail.com>
2016-12-09 14:47:20 -0700

    updated finalized server side scripts

--- ServerSideScripts/addscore.php
+++ ServerSideScripts/addscore.php
@@ -4,17 +4,17 @@
 
 	$username = mysql_real_escape_string($_GET['username'],$db);
 	//$Timestamp = mysql_real_escape_string($_GET['Timestamp'],$db);
-	$bananas = mysql_real_escape_string($_GET['bananas'],$db);
+	$score = mysql_real_escape_string($_GET['score'],$db);
 	$distance = mysql_real_escape_string($_GET['distance'],$db);
-	$score = mysql_real_escape_string($_GET['score']),$db);
+	$bananas = mysql_real_escape_string($_GET['bananas'],$db);
 	$hash = $_GET['hash'];
 
 	$secretkey = "911911911";
 
-	$real_hash = md5($username . $bananas . $distance . $score . $secretkey); //got rid of Timestamp for now
+	$real_hash = md5($username . $score . $distance . $bananas . $secretkey); //got rid of Timestamp for now
 	if($real_hash == $hash){
 		// send variables for the mysql database class
-		$query = "INSERT INTO score_board VALUES ('$username','$bananas','$distance','$score');";
+		$query = "INSERT INTO score_board VALUES ('$username','$score','$distance','$bananas');";
 		$result = mysql_query($query) or die('Query failed: ' .mysql_error());
 	}
 ?>
